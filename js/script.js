83fc9706eff73b33d1b8adbb313ed4e9596ca8c4
arsipstaiis-sketch <arsipstaiis@gmail.com>
2026-09-21 17:18:21 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // KONFIGURASI & GLOBAL VARIABEL
 // ==========================================
 let BIAYA_RUSUM_STANDAR = 6000000;
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwmDTtw_M29bR9fXG2EKQJ2vCy-mmadktVOzMPR-2OAXMH5XsESohjlMEmA6uoM7EsT/exec'; // URL Google Apps Script Anda
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx_fY35sIKSqdG_0bFfvAY8opZPMA_gNcsQWyrFu7ezt5EBXBLOcQdO0djzZlTbBSDI/exec'; // URL Google Apps Script Anda
 
 let transaksiData = []; 
 let mahasiswaMaster = []; 
